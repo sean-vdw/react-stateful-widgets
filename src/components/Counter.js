84cfc6d478c6c6989592dc23ab360860b8025711
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2021-12-06 13:00:42 -0800

    completed tasks 0, 1, 2

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -46,10 +46,11 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from 'react'; /* STEP 0 */
 
 export default function Counter() {
   /* STEP 1 */
+  const [count, setCount] = useState(0);
 
   const increment = () => {
     /* STEP 4 */
@@ -61,10 +62,16 @@ export default function Counter() {
     /* STEP 6 */
   };
 
+  if (count % 2 === 0) {
+    const newColor = 'royalblue';
+  } else {
+    const newColor = 'crimson';
+  }
+
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    color: {newColor} 
   };
 
   return (
